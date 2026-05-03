f0843ee9ec06aabd78257692a50ac77a0a702314
Vinayaknagila <vinayaknagila9@gmail.com>
2026-05-04 01:29:46 +0530

    configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/",// ✅ important for Vercel
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
